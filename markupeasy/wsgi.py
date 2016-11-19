d9250102d2078aa41fb2a60ec16bf21bb68a74c9
Eric Pinet <pineri01@gmail.com>
2016-11-18 22:18:47 -0500

    fix deploy

--- markupeasy/wsgi.py
+++ markupeasy/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/home/pi/MarkupEasy')
 
 from django.core.wsgi import get_wsgi_application
 
